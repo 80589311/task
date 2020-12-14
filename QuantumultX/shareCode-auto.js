a0bbd2f3297760f23d3eb37a978127def4cc8230
KefaTang <67527176+KefaTang@users.noreply.github.com>
2020-12-14 22:24:17 +0800

    Update shareCode-auto.js

--- QuantumultX/shareCode-auto.js
+++ QuantumultX/shareCode-auto.js
@@ -8,18 +8,18 @@
 const $ = new Env("互助码");
 const shareCodes = [
   {
-    zd: $.getdata("zd_shareCode1") || "qu73szyyke7tv4sgpjojpyiq6u",
-    nc: $.getdata("nc_shareCode1") || "a1bc1602151a42d68fb8cf0ee93cc43d",
-    mc: $.getdata("mc_shareCode1") || "MTAxODc2NTEzNDAwMDAwMDAyNzkyNjM4Mw==",
-    ddgc: $.getdata("dd_shareCode1") || "P04z54XCjVWnYaS5kZ7fCKtjCEX",
-    jxgc: $.getdata("jx_shareCode1") || "k3XRgh9SqTEODDhQVrfL1A==",
+    zd: $.getdata("zd_shareCode1") || "wqv6bbkq5emb2h4m6v2or5uzwq3h7wlwy7o5jii",
+    nc: $.getdata("nc_shareCode1") || "09611d3dc4db413b9279212ea86d8240",
+    mc: $.getdata("mc_shareCode1") || "MTAxODc2NTEzNTAwMDAwMDAyODM4NDg2Nw====",
+    ddgc: $.getdata("dd_shareCode1") || "P04z54XCjVWnYaS5jQAC2H52HtPlZGtwmE",
+    jxgc: $.getdata("jx_shareCode1") || "FAznqTJmXlwOt75fhQh_pA==",
   },
   {
-    zd: $.getdata("zd_shareCode2") || "",
-    nc: $.getdata("nc_shareCode2") || "",
-    mc: $.getdata("mc_shareCode2") || "",
-    ddgc: $.getdata("dd_shareCode2") || "",
-    jxgc: $.getdata("jx_shareCode2") || "",
+    zd: $.getdata("zd_shareCode2") || "iu237u55hwjip5uoipu7iw73brgryg3kk357d4i",
+    nc: $.getdata("nc_shareCode2") || "cbcc656dd03a47a59e842d878ee9b16e",
+    mc: $.getdata("mc_shareCode2") || "MTAxODc2NTEzNDAwMDAwMDAzMjI5OTM5Mw==",
+    ddgc: $.getdata("dd_shareCode2") || "P04z54XCjVWnYaS5m9cZwOlvTQK0B9CKeAtSg",
+    jxgc: $.getdata("jx_shareCode2") || "lRPi_71a3LmlY77qhA8E8w==",
   },
 ];
 $.result = [];
